80dbb746dd050bd7eb14814a4717e0bc305554dd
Max <max@imac-max.local>
2019-11-01 15:47:18 +0300

    fixed wrong class name and missing method

--- src/implementations/amoCRM/amoNoteMap.php
+++ src/implementations/amoCRM/amoNoteMap.php
@@ -4,7 +4,10 @@ namespace Kethner\cdcBridge\implementations\amoCRM;
 use Kethner\cdcBridge\interfaces\Map;
 
 
-class amoLead implements Map {
+class amoNoteMap implements Map {
+
+    public static function mapResponse($response) {
+    }
 
     public static function mapRequest($data) {
         return $data;
