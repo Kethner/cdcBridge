d28a57f29a20bcaef8e50d9a6de4e74a768d1895
Kethner <horanzon@gmail.com>
2021-09-02 16:43:32 +0300

    added amo events implementation

---
+++ src/implementations/amoCRM/amoEventMap.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Kethner\cdcBridge\implementations\amoCRM;
+
+use Kethner\cdcBridge\interfaces\Map;
+
+class amoEventMap implements Map
+{
+    public static function mapResponse($response)
+    {
+        $result['id'] = $response['id'];
+        $result['type'] = $response['type'];
+        $result['entity_id'] = $response['entity_id'];
+        $result['entity_type'] = $response['entity_type'];
+        $result['created_by'] = $response['created_by'];
+        $result['created_at'] = $response['created_at'];
+
+        $result['value_after'] = $response['value_after'] ? $response['value_after'][0]['custom_field_value']['text'] : null;
+        $result['value_before'] = $response['value_before'] ? $response['value_before'][0]['custom_field_value']['text'] : null;
+
+        return $result;
+    }
+
+    public static function mapRequest($data)
+    {
+    }
+}
---
+++ src/implementations/amoCRM/amoEvents.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Kethner\cdcBridge\implementations\amoCRM;
+
+use Kethner\cdcBridge\interfaces\Connector;
+
+class amoEvents implements Connector
+{
+
+    public $connection;
+
+    function __construct(amoConnection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+
+    public function get($data_object)
+    {
+        $data = &$data_object->data;
+        $response = $this->connection->request(null, 'api/v4/events?limit=' . $data['limit'] . '&page=' . $data['page'] . "&filter[type]=custom_field_937811_value_changed");
+        if (empty($response)) return false;
+
+        $response = $response['_embedded']['events'];
+        foreach ($response as $item) {
+            $data[] = $this->map::mapResponse($item);
+        }
+
+        return true;
+    }
+
+    public function set($data_object)
+    {
+    }
+}
